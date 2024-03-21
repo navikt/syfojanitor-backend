b1e5b2c15abc7579f97020b6ab9f350271de9a8d
geir-waagboe <geir.arne.waagbo@nav.no>
2024-03-21 12:18:46 +0100

    Initial commit

--- build/resources/main/db/migration/R__grant_to_cloudsqliamuser.sql
+++
@@ -1,2 +0,0 @@
-REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
--- build/resources/main/db/migration/V1_1__create_user_cloudsqliamuser.sql
+++
@@ -1,7 +0,0 @@
-DO $$
-BEGIN
-  CREATE ROLE cloudsqliamuser WITH NOLOGIN;
-  EXCEPTION WHEN DUPLICATE_OBJECT THEN
-  RAISE NOTICE 'not creating role cloudsqliamuser -- it already exists';
-END
-$$;
